6576fdd96605a1d72d4613b8fc39225f79063986
AMT94-star <aslihan.taskin94@gmail.com>
2026-04-30 11:57:29 +0200

    Created controller, service, updates to pom

--- src/main/java/se/amt/googlegeminilabb/controller/GeminiController.java
+++ src/main/java/se/amt/googlegeminilabb/controller/GeminiController.java
@@ -1,4 +1,24 @@
 package se.amt.googlegeminilabb.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import se.amt.googlegeminilabb.service.GeminiService;
+
+@Controller
+@RequestMapping("/")
 public class GeminiController {
+    private final GeminiService geminiService;
+
+    public GeminiController(GeminiService geminiService) {
+        this.geminiService = geminiService;
+    }
+
+    @ResponseBody
+    @GetMapping("/request")
+    public String request(@RequestParam String prompt) {
+        return geminiService.ask(prompt);
+    }
 }
--- src/main/java/se/amt/googlegeminilabb/service/GeminiService.java
+++ src/main/java/se/amt/googlegeminilabb/service/GeminiService.java
@@ -1,4 +1,45 @@
 package se.amt.googlegeminilabb.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class GeminiService {
+
+    public String ask(@RequestParam String prompt) {
+        String api = System.getenv("GOOGLE_API_KEY");
+
+        String url =
+                "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + api;
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> body = Map.of(
+                "contents", List.of(
+                        Map.of(
+                                "parts", List.of(
+                                        Map.of("text", prompt)
+                                )
+                        )
+                )
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(url, request, String.class);
+
+        return response.getBody();
+    }
 }
